dbba4be5e62bf384e016553e27df87f80477c439
karahandayi <karahandayi9@gmail.com>
2026-02-16 23:48:49 +0100

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const map = L.map("map", {
-  zoomControl: false
-}).setView([39.0, 35.0], 6); // Türkiye merkez
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "&copy; OpenStreetMap"
-}).addTo(map);
