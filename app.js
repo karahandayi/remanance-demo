60d2fca45312d40ba1b71e9b13cd0fc76189d62f
karahandayi <karahandayi9@gmail.com>
2026-02-17 02:25:58 +0100

    app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const map = L.map("map", {
   attributionControl: false
 }).setView([39.0, 35.0], 6);
 
-// 🔴 KARANLIK HARİTA (CARTO DARK)
 L.tileLayer(
   "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
   {
